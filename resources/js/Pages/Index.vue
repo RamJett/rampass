a9ebc5e1b2ff4928c960407a2f680848284b1d18
RamJett <ramjett@ramnic.com>
2022-08-02 08:48:42 -0700

    just a placeholder

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Head, Link } from '@inertiajs/inertia-vue3';
+
+</script>
+
+<template>
+    <Head title="Welcome" />
+
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+
+    </div>
+</template>
