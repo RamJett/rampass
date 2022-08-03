0005edd10b03e357c560a33eef9113c99d62641b
RamJett <ramjett@ramnic.com>
2022-08-03 09:51:35 -0700

    simple stuff -- start with a textarea

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -4,9 +4,33 @@ import { Head, Link } from '@inertiajs/inertia-vue3';
 </script>
 
 <template>
-    <Head title="Welcome" />
-
-    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-
-    </div>
+<div class="flex justify-center">
+  <div class="mb-3 xl:w-96">
+    <label for="secret" class="form-label inline-block mb-3 text-gray-700"
+      >Secret</label
+    >
+    <textarea
+      class="
+        form-control
+        block
+        w-full
+        px-2
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+      "
+      id="secret"
+      rows="3"
+      placeholder="Your secret"
+    ></textarea>
+  </div>
+</div>
 </template>
