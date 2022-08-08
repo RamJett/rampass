da3c6a699f32d904a277e6618b7e1b5856a17e06
RamJett <ramjett@ramnic.com>
2022-08-08 14:16:57 -0700

    semi-formatted

--- resources/js/Pages/Create.vue
+++ resources/js/Pages/Create.vue
@@ -1,7 +1,7 @@
 <script setup>
-import { Head, Link, useForm } from "@inertiajs/inertia-vue3";
-import Layout from "@/App/Layout.vue";
-import AppButton from "@/App/Button.vue";
+import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
+import Layout from '@/App/Layout.vue';
+import AppButton from '@/App/Button.vue';
 
 const form = useForm({
   secret: null,
@@ -11,11 +11,11 @@ const form = useForm({
 });
 
 const submit = () => {
-  form.post(route("secret.store"));
+  form.post(route('secret.store'));
 };
 
 const props = defineProps({
-  units: Object,
+  units: Array,
   units_default: String,
   time: Number,
   views: Number,
@@ -24,24 +24,168 @@ const props = defineProps({
 
 <template>
   <Layout title="Create">
-    <form @submit.prevent="submit">
-      <textarea v-model="form.secret" />
+    <div class="flex items-center justify-center p-4">
+      <form @submit.prevent="submit" class="w-full max-w-lg">
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full px-3">
+            <label
+              class="
+                block
+                uppercase
+                tracking-wide
+                text-gray-700 text-xs
+                font-bold
+                mb-2
+              "
+              for="secret"
+            >
+              Secret
+            </label>
+            <textarea
+              v-model="form.secret"
+              class="
+                appearance-none
+                block
+                w-full
+                bg-gray-200
+                text-gray-700
+                border border-red-500
+                rounded
+                py-3
+                px-4
+                mb-3
+                leading-tight
+                focus:outline-none focus:bg-white
+              "
+              id="secret"
+              placeholder="Type secret here"
+            />
+          </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label
+              class="
+                block
+                uppercase
+                tracking-wide
+                text-gray-700 text-xs
+                font-bold
+                mb-2
+              "
+              for="time"
+            >
+              Time
+            </label>
+            <input
+              v-model="form.time"
+              class="
+                appearance-none
+                block
+                w-full
+                bg-gray-200
+                text-gray-700
+                border border-red-500
+                rounded
+                py-3
+                px-4
+                mb-3
+                leading-tight
+                focus:outline-none focus:bg-white
+              "
+              id="time"
+              type="number"
+              min="1"
+            />
+          </div>
+          <div class="w-full md:w-1/2 px-3">
+            <label
+              class="
+                block
+                uppercase
+                tracking-wide
+                text-gray-700 text-xs
+                font-bold
+                mb-2
+              "
+              for="units"
+            >
+              Units
+            </label>
+            <select
+              v-model="form.units"
+              class="
+                appearance-none
+                block
+                w-full
+                bg-gray-200
+                text-gray-700
+                border border-red-500
+                rounded
+                py-3
+                px-4
+                leading-tight
+                focus:outline-none focus:bg-white focus:border-gray-500
+              "
+              id="units"
+            >
+              <option
+                v-for="unit in props.units"
+                :value="unit.name"
+                :key="unit.name"
+              >
+                {{ unit.name }}
+              </option>
+            </select>
+          </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full px-3">
+            <label
+              class="
+                block
+                uppercase
+                tracking-wide
+                text-gray-700 text-xs
+                font-bold
+                mb-2
+              "
+              for="views"
+            >
+              Views
+            </label>
 
-      <input type="number" v-model="form.time" />
-
-      <select v-model="form.units">
-        <option v-for="unit in props.units" :value="unit.name" :key="unit.name">
-          {{ unit.name }}
-        </option>
-      </select>
-
-      <input type="number" v-model="form.views" />
-      <AppButton
-        :class="{ 'opacity-25': form.processing }"
-        :disabled="form.processing"
-      >
-        submit
-      </AppButton>
-    </form>
+            <input
+              type="number"
+              v-model="form.views"
+              id="views"
+              min="1"
+              class="
+                appearance-none
+                block
+                w-full
+                bg-gray-200
+                text-gray-700
+                border border-gray-200
+                rounded
+                py-3
+                px-4
+                mb-3
+                leading-tight
+                focus:outline-none focus:bg-white focus:border-gray-500
+              "
+            />
+          </div>
+        </div>
+        <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+          <AppButton
+            :class="{ 'opacity-25': form.processing }"
+            :disabled="form.processing"
+          >
+            submit
+          </AppButton>
+        </div>
+      </form>
+    </div>
   </Layout>
 </template>
