e3d8ac09cfeb890a6c25046a81e03b4c4442fa06
RamJett <ramjett@ramnic.com>
2022-08-09 16:55:22 -0700

    WIP

--- resources/js/Pages/Show.vue
+++ resources/js/Pages/Show.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { Head, Link, useForm } from "@inertiajs/inertia-vue3";
-import AppButton from "@/App/Button.vue";
+import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
+import AppButton from '@/App/Button.vue';
 import Layout from '@/App/Layout.vue';
 
 const form = useForm({
@@ -17,49 +17,53 @@ defineProps({
 </script>
 
 <template>
-<Layout>
-  <form @submit.prevent="submit" class="p-4">
-    <div class="flex justify-center">
-      <div class="mb-3 xl:w-96">
-        <label for="secret" class="form-label inline-block mb-3 text-gray-700"
-          >Secret</label
-        >
+  <Layout>
+    <form @submit.prevent="submit" class="p-4">
+      <div class="flex justify-center">
+        <div class="mb-3 xl:w-96">
+          <label
+            for="secret"
+            class="
+              block
+              uppercase
+              tracking-wide
+              text-gray-700 text-xs
+              font-bold
+              mb-2
+            "
+            >Secret</label
+          >
 
-        <textarea
-          v-model="secret"
-          class="
-            form-control
-            block
-            w-full
-            px-2
-            py-1.5
-            text-base
-            font-normal
-            text-gray-700
-            bg-white bg-clip-padding
-            border border-solid border-gray-300
-            rounded
-            transition
-            ease-in-out
-            m-0
-            focus:text-gray-700
-            focus:bg-white
-            focus:border-blue-600
-            focus:outline-none
-          "
-          id="secret"
-          rows="3"
-          placeholder="Your secret"
-        ></textarea>
-        <AppButton
-          class="ml-1 mt-2"
-          :class="{ 'opacity-25': form.processing }"
-          :disabled="form.processing"
-        >
-          Copy
-        </AppButton>
+          <textarea
+            v-model="secret"
+            readonly="true"
+            class="
+              appearance-none
+              block
+              w-full
+              bg-gray-200
+              text-gray-700
+              border border-blue-500
+              rounded
+              py-3
+              px-4
+              mb-3
+              leading-tight
+              focus:outline-none focus:bg-white
+            "
+            id="secret"
+            rows="3"
+            placeholder="Your secret"
+          ></textarea>
+          <AppButton
+            class="ml-1 mt-2"
+            :class="{ 'opacity-25': form.processing }"
+            :disabled="form.processing"
+          >
+            Copy
+          </AppButton>
+        </div>
       </div>
-    </div>
-  </form>
-</Layout>
+    </form>
+  </Layout>
 </template>
