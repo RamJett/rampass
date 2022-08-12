0e566074053900ea4cf9cd50e6f14f5b31fbb3bc
RamJett <ramjett@ramnic.com>
2022-08-12 09:26:58 -0700

    format

--- resources/js/Pages/Create.vue
+++ resources/js/Pages/Create.vue
@@ -29,68 +29,71 @@ const props = defineProps({
 <template>
   <Layout title="Create">
     <div class="flex items-center justify-center p-4">
-      <form @submit.prevent="submit" class="w-full max-w-lg">
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full px-3">
-            <Label for="secret" value="Secret" class="mb-2" />
-            <Textarea
-              v-model="form.secret"
-              id="secret"
-              placeholder="Type secret here"
-              class="w-full py-3 px-4 mb-3"
-            />
+      <div class="w-full max-w-lg">
+        <form @submit.prevent="submit">
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+              <Label for="secret" value="Secret" class="mb-2" />
+              <Textarea
+                v-model="form.secret"
+                id="secret"
+                placeholder="Type secret here"
+                class="w-full py-3 px-4 mb-3"
+                required
+              />
+            </div>
           </div>
-        </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <Label for="time" value="Time" class="mb-2" />
-            <Input
-              v-model="form.time"
-              type="number"
-              min="1"
-              id="time"
-              class="py-3 px-4 mb-3"
-              required
-            />
-          </div>
-          <div class="w-full md:w-1/2 px-3">
-            <Label for="units" value="Units" class="mb-2" />
-            <Select
-              v-model="form.units"
-              id="units"
-              class="w-full py-3 px-4 mb-3"
-            >
-              <option
-                v-for="unit in props.units"
-                :value="unit.name"
-                :key="unit.name"
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <Label for="time" value="Time" class="mb-2" />
+              <Input
+                v-model="form.time"
+                type="number"
+                min="1"
+                id="time"
+                class="w-full py-3 px-4 mb-3"
+                required
+              />
+            </div>
+            <div class="w-full md:w-1/2 px-3">
+              <Label for="units" value="Units" class="mb-2" />
+              <Select
+                v-model="form.units"
+                id="units"
+                class="w-full py-3 px-4 mb-3"
               >
-                {{ unit.name }}
-              </option>
-            </Select>
+                <option
+                  v-for="unit in props.units"
+                  :value="unit.name"
+                  :key="unit.name"
+                >
+                  {{ unit.name }}
+                </option>
+              </Select>
+            </div>
           </div>
-        </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full px-3">
-            <Label for="views" value="Views" class="mb-2" />
-            <Input
-              v-model="form.views"
-              id="views"
-              type="number"
-              min="1"
-              class="w-full py-3 px-4 mb-3"
-              required
-            />
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+              <Label for="views" value="Views" class="mb-2" />
+              <Input
+                v-model="form.views"
+                id="views"
+                type="number"
+                min="1"
+                class="w-full py-3 px-4 mb-3"
+                required
+              />
+            </div>
           </div>
-        </div>
-        <AppButton
-          class="ml-0"
-          :class="{ 'opacity-25': form.processing }"
-          :disabled="form.processing"
-        >
-          Create
-        </AppButton>
-      </form>
+          <AppButton
+            class="ml-0"
+            :class="{ 'opacity-25': form.processing }"
+            :disabled="form.processing"
+          >
+            Create
+          </AppButton>
+        </form>
+      </div>
     </div>
   </Layout>
 </template>
