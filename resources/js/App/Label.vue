bbb832e6f3d0ed5765591d1817db48455a975eb6
RamJett <ramjett@ramnic.com>
2022-08-11 17:02:46 -0700

    initial commit

---
+++ resources/js/App/Label.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  value: String,
+});
+</script>
+
+<template>
+  <label class="block uppercase tracking-wide font-bold text-gray-700">
+    <span v-if="value">{{ value }}</span>
+    <span v-else><slot /></span>
+  </label>
+</template>
