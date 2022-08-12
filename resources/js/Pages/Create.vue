207934046c2f7ea9974f971d78756dfd06fe3cb5
RamJett <ramjett@ramnic.com>
2022-08-11 17:02:33 -0700

    use Label component

--- resources/js/Pages/Create.vue
+++ resources/js/Pages/Create.vue
@@ -2,6 +2,7 @@
 import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
 import Layout from '@/App/Layout.vue';
 import AppButton from '@/App/Button.vue';
+import Label from '@/App/Label.vue';
 
 const form = useForm({
   secret: null,
@@ -28,19 +29,7 @@ const props = defineProps({
       <form @submit.prevent="submit" class="w-full max-w-lg">
         <div class="flex flex-wrap -mx-3 mb-6">
           <div class="w-full px-3">
-            <label
-              class="
-                block
-                uppercase
-                tracking-wide
-                text-gray-700 text-xs
-                font-bold
-                mb-2
-              "
-              for="secret"
-            >
-              Secret
-            </label>
+            <Label for="secret" value="Secret" class="mb-2" />
             <textarea
               v-model="form.secret"
               class="
@@ -64,19 +53,7 @@ const props = defineProps({
         </div>
         <div class="flex flex-wrap -mx-3 mb-6">
           <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label
-              class="
-                block
-                uppercase
-                tracking-wide
-                text-gray-700 text-xs
-                font-bold
-                mb-2
-              "
-              for="time"
-            >
-              Time
-            </label>
+            <Label for="time" value="Time" class="mb-2" />
             <input
               v-model="form.time"
               class="
@@ -99,19 +76,7 @@ const props = defineProps({
             />
           </div>
           <div class="w-full md:w-1/2 px-3">
-            <label
-              class="
-                block
-                uppercase
-                tracking-wide
-                text-gray-700 text-xs
-                font-bold
-                mb-2
-              "
-              for="units"
-            >
-              Units
-            </label>
+            <Label for="units" value="Units" class="mb-2" />
             <select
               v-model="form.units"
               class="
@@ -142,19 +107,7 @@ const props = defineProps({
         </div>
         <div class="flex flex-wrap -mx-3 mb-6">
           <div class="w-full px-3">
-            <label
-              class="
-                block
-                uppercase
-                tracking-wide
-                text-gray-700 text-xs
-                font-bold
-                mb-2
-              "
-              for="views"
-            >
-              Views
-            </label>
+            <Label for="views" value="Views" class="mb-2" />
 
             <input
               type="number"
