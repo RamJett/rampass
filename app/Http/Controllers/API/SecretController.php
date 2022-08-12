8e03ca266680b5b298190271e3bfac6e2cc888a4
RamJett <ramjett@ramnic.com>
2022-08-12 16:54:34 -0700

    add store and show

--- app/Http/Controllers/API/SecretController.php
+++ app/Http/Controllers/API/SecretController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use App\Models\Secret;
 use App\Models\Maintenance;
-use Inertia\Inertia;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Str;
 use Carbon\Carbon;
@@ -16,6 +16,29 @@ class SecretController extends Controller
   public function store(Request $request)
   {
     // TODO: validation
+
+    // First make sure content-type is application/json
+    $request->merge([
+      'content_type' => Str::lower($request->header('Content-Type')),
+    ]);
+
+    $validator = Validator::make($request->all(), [
+      'content_type' => 'required|starts_with:application/json',
+      'time' => 'required|integer|gte:1',
+      'views' => 'required|integer|gte:1',
+      'units' => 'required|in:minutes,hours,days',
+      'secret' => 'required',
+    ]);
+
+    if ($validator->fails()) {
+      return response()->json(
+        [
+          'status' => 'fail',
+          'errors' => $validator->errors()->all(),
+        ],
+        422
+      );
+    }
 
     $uuid = Str::uuid();
     $now = Carbon::now();
@@ -38,8 +61,62 @@ class SecretController extends Controller
     ]);
 
     $expires_at = $secret->expires_at;
-    $views_remaining = $secret->expires_views - $secret->count_views;
+    $views_remaining = $secret->expire_views - $secret->count_views;
 
-    return 'Testing';
+    return response()->json([
+      'status' => 'success',
+      'data' => [
+        'url' => env('APP_URL') . '/' . $uuid,
+        'expires_at' => $expires_at,
+        'views_remaining' => $views_remaining,
+      ],
+    ]);
+  }
+
+  public function show($uuid)
+  {
+    $secret = Secret::where(
+      'uuid',
+      crypt($uuid, '$6$rounds=5000$' . env('APP_SALT') . '$')
+    )->first();
+
+    if (!empty($secret->secret)) {
+      // Check for expiry
+      if (
+        Carbon::now()->gte($secret->expires_at) ||
+        $secret->count_views >= $secret->expire_views
+      ) {
+        $secret->delete();
+        return response()->json(
+          [
+            'status' => 'fail',
+          ],
+          404
+        );
+      } else {
+        // Increment the view count
+        $secret->increment('count_views');
+
+        $secretDecrypted = Crypt::decryptString($secret->secret);
+        $expires_at = $secret->expires_at;
+        $views_remaining = $secret->expire_views - $secret->count_views;
+      }
+    } else {
+      return response()->json(
+        [
+          'status' => 'fail',
+        ],
+        404
+      );
+    }
+
+    return response()->json([
+      'status' => 'success',
+      'data' => [
+        'secret' => $secretDecrypted,
+        'expires_at' => $expires_at,
+        'views_remaining' => $views_remaining,
+      ],
+    ]);
   }
 }
