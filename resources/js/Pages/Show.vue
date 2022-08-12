fb59deac4af2bf892f1bb48083cd3ca0e286661e
RamJett <ramjett@ramnic.com>
2022-08-12 09:26:13 -0700

    use Textarea.vue

--- resources/js/Pages/Show.vue
+++ resources/js/Pages/Show.vue
@@ -3,6 +3,7 @@ import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
 import AppButton from '@/App/Button.vue';
 import Layout from '@/App/Layout.vue';
 import Label from '@/App/Label.vue';
+import Textarea from '@/App/Textarea.vue';
 
 const useClipboard = (text) => {
   let supported = navigator && 'clipboard' in navigator;
@@ -25,27 +26,14 @@ defineProps({
       <div class="flex justify-center">
         <div class="mb-3 xl:w-96">
           <Label for="secret" value="Secret" class="mb-2" />
-          <textarea
+          <Textarea
             v-model="secret"
             readonly="true"
-            class="
-              appearance-none
-              block
-              w-full
-              bg-gray-200
-              text-gray-700
-              border border-blue-500
-              rounded
-              py-3
-              px-4
-              mb-3
-              leading-tight
-              focus:outline-none focus:bg-white
-            "
+            class="w-full py-3 px-4 mb-3"
             id="secret"
             rows="3"
             placeholder="Your secret"
-          ></textarea>
+          />
           <AppButton class="ml-0 mt-2" @click="useClipboard(secret)">
             Copy
           </AppButton>
