04ecc232894f442a0d922c42dee68c15777754fa
RamJett <ramjett@ramnic.com>
2022-08-15 06:57:13 -0700

    initial commit

---
+++ resources/js/App/LayoutLight.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { Head } from '@inertiajs/inertia-vue3';
+import TopNav from '@/App/TopNav.vue';
+
+defineProps({
+  title: String,
+});
+</script>
+
+<template>
+  <Head :title="title" />
+  <section class="px-6 py-8">
+    <TopNav />
+  </section>
+  <header class="max-w-4xl mx-auto mt-10 text-center">
+    <div class="max-w-xl mx-auto">
+      <h1 class="text-4xl"><slot name="header" /></h1>
+    </div>
+  </header>
+  <main><slot name="main" /></main>
+  <footer></footer>
+</template>
---
+++ resources/js/App/TopNav.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+  <nav class="flex justify-between items-center">
+    <div>
+      <Link href="#">
+        <span class="text-xs font-bold uppercase mr-3">RAMPASS</span>
+      </Link>
+    </div>
+    <div>
+      <Link href="/" class="text-xs font-bold uppercase">Create</Link>
+      <Link href="/about" class="text-xs font-bold uppercase ml-3">About</Link>
+    </div>
+  </nav>
+</template>
