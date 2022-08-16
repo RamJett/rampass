394e4a9a1a1c3e2704f4350a0c877da9aa9734d9
RamJett <ramjett@ramnic.com>
2022-08-15 17:28:35 -0700

    Change Link to use route names

--- resources/js/App/TopNav.vue
+++ resources/js/App/TopNav.vue
@@ -5,12 +5,12 @@ import { Link } from '@inertiajs/inertia-vue3';
 <template>
   <nav class="flex justify-between items-center">
     <div>
-      <Link href="#">
+      <Link :href="route('secret.create')">
         <span class="text-xs font-bold uppercase mr-3">RAMPASS</span>
       </Link>
     </div>
     <div>
-      <Link href="/" class="text-xs font-bold uppercase">Create</Link>
+      <Link :href="route('secret.create')" class="text-xs font-bold uppercase">Create</Link>
       <Link href="/about" class="text-xs font-bold uppercase ml-3">About</Link>
     </div>
   </nav>
