112322dde21bccc303083fc7383db7cc97139969
RamJett <ramjett@ramnic.com>
2022-08-26 15:50:28 -0700

    format

--- resources/js/App/TopNav.vue
+++ resources/js/App/TopNav.vue
@@ -3,19 +3,26 @@ import { Link } from '@inertiajs/inertia-vue3';
 </script>
 
 <template>
-  <nav class="flex justify-between items-center">
-    <div>
-      <Link :href="route('secret.create')">
-        <span class="text-xs font-bold uppercase mr-3">RAMPASS</span>
-      </Link>
-    </div>
-    <div>
-      <Link :href="route('secret.create')" class="text-xs font-bold uppercase">
-        Create
-      </Link>
-      <Link href="/about" class="text-xs font-bold uppercase ml-3">
-        About
-      </Link>
-    </div>
-  </nav>
+  <div>
+    <nav class="flex justify-between items-center">
+      <div>
+        <Link :href="route('secret.create')">
+          <span class="text-xs font-bold uppercase tracking-wide mr-3"
+            >RAMPASS</span
+          >
+        </Link>
+      </div>
+      <div>
+        <Link
+          :href="route('secret.create')"
+          class="text-xs font-bold uppercase"
+        >
+          Create
+        </Link>
+        <Link href="/about" class="text-xs font-bold uppercase ml-3">
+          About
+        </Link>
+      </div>
+    </nav>
+  </div>
 </template>
