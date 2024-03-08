9c098477119782c6c6f3cfa6610a2510ec807643
RamJett <ramjett@ramnic.com>
2024-03-08 11:14:05 -0800

    initial commit

---
+++ resources/js/App/NavLink.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { computed } from 'vue';
+import { Link } from '@inertiajs/vue3';
+
+const props = defineProps({
+    href: String,
+    active: Boolean,
+});
+
+const classes = computed(() => {
+    return props.active
+        ? '"block px-3 py-2 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page"'
+        : '"block px-3 py-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"';
+});
+</script>
+
+<template>
+    <Link :href="href" :class="classes">
+        <slot />
+    </Link>
+</template>
