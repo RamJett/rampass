6a71e49f1f4fe7428cc9cb85ac02abf331986470
RamJett <ramjett@ramnic.com>
2024-03-10 12:32:31 -0700

    first real placeholder for about page
    more later

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -1,11 +1,51 @@
 <script setup>
 import Layout from "@/App/Layout.vue";
+import AppButton from "@/App/Button.vue";
+
+const github = () => {
+  window.open(route("about.togithub"), "_blank")
+};
 </script>
 
 <template>
   <Layout title="About">
-  <template #header>RAMPASS</template>
     <template #main>
+      <div class="flex justify-center">
+        <div
+          class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+        >
+          <a href="#">
+            <h5
+              class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+            >
+              RAMPASS
+            </h5>
+          </a>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Share your secrets in a more secure way.
+          </p>
+          <AppButton @click="github"
+            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            See on github
+            <svg
+              class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </appButton>
+        </div>
+      </div>
     </template>
   </Layout>
 </template>
